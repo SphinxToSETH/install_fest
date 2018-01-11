c0352031fc911a4132d1f0c15300f7408f4a4a00
SphinxToSETH <31757811+SphinxToSETH@users.noreply.github.com>
2018-01-11 01:51:37 +0100

    Update 1_rvm_ruby_rails_nodejs_bundle.sh

--- 1_rvm_ruby_rails_nodejs_bundle.sh
+++ 1_rvm_ruby_rails_nodejs_bundle.sh
@@ -6,6 +6,7 @@ clear
 /bin/echo -e "\e[36mIMPORTANT FAIRE LE RUN AT LOGON DANS LES PREFERENCES DU TERMINAL SINON CEST MORT !!!\e[0m"
 sleep 3
 sudo rm /var/lib/dpkg/lock
+/bin/echo -e "\e[36mInstall de curl\e[0m"
 sudo apt-get -y install  curl
 
 
@@ -27,9 +28,10 @@ sudo chmod 600 *
 #LIB
 sudo apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
 #DL KEY
-/bin/echo -e "\e[36mTelechargement de la clef\e[0m"
+/bin/echo -e "\e[36mTelechargement de la clef pour l install de rvm\e[0m"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 #DL RVM
+/bin/echo -e "\e[36mTelechargement de rvm\e[0m"
 curl -sSL https://get.rvm.io | bash -s stable
 #UPDATE SOURCE
 /bin/echo -e "\e[36mUpdate >>  To start using RVM you need to run `source /etc/profile.d/rvm.sh`\e[0m"
